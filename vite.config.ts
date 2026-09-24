c336242c3ec6a080975a9d246b3c541de7e49e91
deva <devasharjin@gmail.com>
2026-09-24 19:24:57 +0530

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ], base: '/fairgigs/',
+  ],
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
