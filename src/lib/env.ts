233528f1ffdf03162b40f92f42ab8af7ff145a81
deva <devasharjin@gmail.com>
2026-09-24 18:37:57 +0530

    updated

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,3 +1,3 @@
 export const env = {
-    backendUrl: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+  backendUrl: import.meta.env.VITE_BACKEND_URL || "https://sih-vackend-live.vercel.app",
 };
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ],
+  ], base: '/fairgigs/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
